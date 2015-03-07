85140649980abeec336abe0044130b7b93ff42b2
Kevin Hardy <hardy95k@yahoo.com>
2015-03-06 20:52:43 -0700

    added login folder

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 var app = angular.module('rtfmApp', ['firebase', 'ngRoute']);
 
+app.config(function($routeProvider) {
+
+	$routeProvider
+	.when('/login', {
+		templateUrl: 'login/login.html',
+		controller: 'loginController'
+	})
+	.when('/threads', {
+		templateUrl:
+		controller:
+	})
+	.when('/threads/:threadId', {
+		templateUrl:
+		controller:
+	})
+	.otherwise({
+		redirectTo: '/login'
+	})
+
+});
